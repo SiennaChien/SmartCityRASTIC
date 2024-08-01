facc04b44dfc6c08a2f855328b07aafc909e90a3
SiennaChien <sienna.chien@gmail.com>
2024-08-01 15:41:44 -0400

    completed individual PID function

--- Phase 3.5, Unique PID/Limo_project/catkin_ws/src/cav_project/src/cav_class.py
+++ Phase 3.5, Unique PID/Limo_project/catkin_ws/src/cav_project/src/cav_class.py
@@ -10,6 +10,7 @@ from ackermann_msgs.msg import AckermannDrive
 from scipy.integrate import odeint
 from cvxopt import matrix, solvers
 from cav_project.msg import limo_info, QP_solution, ControlInfo
+from set_map import set_map
 
 
 class CAV:
@@ -91,29 +92,7 @@ class CAV:
         self.velocity = self.cav_info.vel.data
 
     def generate_map(self, isMain, enter, exit):
-        #coordinates of every critical point
-        self.lane_width = 450
-        self.pt_a = (2933, -1980)
-        self.pt_b = (3044, 2536)
-        self.pt_c = (1972, -1977)
-        self.pt_d = (1877, -624)
-        self.pt_e = (2040, 2519)
-        self.pt_f = (522, 2507)
-        self.pt_g = (30, 270)
-        self.pt_h = (413, 688)
-        self.pt_i = (1560, -1945)
-        self.pt_j = (-2012, -1812)
-        self.pt_k = (-2003, 255)
-        self.pt_l = (-2021, 823)
-        self.pt_m = (-2020, 2544)
-        self.pt_n = (-2412, -1788)
-        self.pt_o = (-2402, 121)
-        self.pt_p = (-2546, 686)
-        self.pt_q = (-2408, 2522)
-        self.pt_r = (-4683, -1980)
-        self.pt_s = (-4668, 274)
-        self.pt_t = (-4578, 683)
-        self.pt_u = (-4660, 2654)
+        set_map(self, self.ID)
 
         #equations for each line, in the A B C form, each variable is a tuple (A, B, C)
         self.path_A = self.generate_line(self.pt_a, self.pt_r)
@@ -127,90 +106,6 @@ class CAV:
         self.path_I = self.generate_line(self.pt_g, self.pt_s)
         self.path_J = self.generate_line(self.pt_h, self.pt_t)
         self.path_K = self.generate_line(self.pt_r, self.pt_u)
-
-            #data points that characterize each circle for the corners - center x, center y, radius. Each variable is a tuple (A, B, C)
-        self.circle_a = (self.pt_a[0] - self.lane_width, self.pt_a[1] + self.lane_width, self.lane_width/2)
-        self.circle_b = (self.pt_b[0] - self.lane_width, self.pt_b[1] - self.lane_width, self.lane_width)
-        self.circle_c = (self.pt_c[0] + self.lane_width, self.pt_c[1] + self.lane_width, self.lane_width/1.8)
-        self.circle_d = (self.pt_d[0] + self.lane_width, self.pt_d[1] - self.lane_width, self.lane_width) #in practice this is not use
-        self.circle_e = (self.pt_e[0] + self.lane_width, self.pt_e[1] - self.lane_width, self.lane_width)
-        self.circle_f = (self.pt_f[0] + self.lane_width, self.pt_f[1] - self.lane_width, self.lane_width/2)
-        self.circle_g = (self.pt_d[0] - self.lane_width, self.pt_d[1] - self.lane_width, self.lane_width)
-        self.circle_h = (self.pt_h[0] - self.lane_width, self.pt_h[1] - self.lane_width, self.lane_width)
-        self.circle_i = (self.pt_i[0] + self.lane_width, self.pt_i[1] + self.lane_width, self.lane_width)
-        self.circle_j = (self.pt_j[0] + self.lane_width, self.pt_j[1] + self.lane_width, self.lane_width)
-        self.circle_k = (self.pt_k[0] + self.lane_width*2.5, self.pt_k[1] - self.lane_width*2, self.lane_width*2)
-        self.circle_l = (self.pt_l[0] + self.lane_width*2.8, self.pt_l[1] + self.lane_width*2, self.lane_width*2)
-        self.circle_m = (self.pt_m[0] + self.lane_width, self.pt_m[1] - self.lane_width, self.lane_width)
-        self.circle_n = (self.pt_n[0] - self.lane_width, self.pt_n[1] + self.lane_width, self.lane_width)
-        self.circle_o = (self.pt_o[0] - self.lane_width*2.9, self.pt_o[1] - self.lane_width*2, self.lane_width*2.1)
-        self.circle_p = (self.pt_p[0] - self.lane_width*2.2, self.pt_p[1] + self.lane_width*2.2, self.lane_width*1.9) #smaller multiplier to x increases x & decrease the multiplier to increase z
-        self.circle_q = (self.pt_q[0] - self.lane_width, self.pt_q[1] - self.lane_width, self.lane_width)
-        self.circle_r = (self.pt_r[0] + self.lane_width, self.pt_r[1] + self.lane_width, self.lane_width)
-        self.circle_s = (self.pt_s[0] + self.lane_width, self.pt_s[1] - self.lane_width, self.lane_width)
-        self.circle_t = (self.pt_t[0] + self.lane_width, self.pt_t[1] + self.lane_width, self.lane_width)
-        self.circle_u = (self.pt_u[0] + self.lane_width, self.pt_u[1] - self.lane_width, self.lane_width)
-
-        #the ranges near each corner that activates the circle path for the limo to follow
-        self.act_range_a = (self.lane_width *1.1, self.lane_width/1.6)
-        self.act_range_b = (self.lane_width /0.9, self.lane_width*1.3)
-        self.act_range_c = (self.lane_width / 1.5, self.lane_width * 1.5)
-        self.act_range_d = (self.lane_width * 1, self.lane_width /2)
-        self.act_range_e = (self.lane_width * 1.1, self.lane_width /2)
-        self.act_range_f = (self.lane_width * 1.2, self.lane_width)
-        self.act_range_g = (self.lane_width * 1, self.lane_width * 1)
-        self.act_range_h = (self.lane_width * 1, self.lane_width * 1)
-        self.act_range_i = (self.lane_width * 1, self.lane_width * 1)
-        self.act_range_j = (self.lane_width * 1, self.lane_width * 1)
-        self.act_range_k = (self.lane_width * 1.5, self.lane_width * 1.5)
-        self.act_range_l = (self.lane_width *1.2, self.lane_width *1.2)
-        self.act_range_m = (self.lane_width * 1, self.lane_width * 1)
-        self.act_range_n = (self.lane_width * 1, self.lane_width * 1)
-        self.act_range_o = (self.lane_width * 1., self.lane_width * 1)
-        self.act_range_p = (self.lane_width * 1.25, self.lane_width * 1.25)
-        self.act_range_q = (self.lane_width * 1.3, self.lane_width * 1.3)
-        self.act_range_r = (self.lane_width * 1, self.lane_width * 1)
-        self.act_range_s = (self.lane_width * 1, self.lane_width * 1)
-        self.act_range_t = (self.lane_width * 1.3, self.lane_width * 1.3)
-        self.act_range_u = (self.lane_width * 1, self.lane_width * 1)
-
-        #PID values of each line, each element is a tuple (kp, ki, kd)
-        self.path_A_PID = (0.0005, 0.00005, 0.001)
-        self.path_B_PID = (0.0009, 0.00005, 0.001)
-        self.path_C_PID = (-0.0008, -0.00008, -0.005)
-        self.path_D_PID = (-0.0007, -0.00009, -0.0015)
-        #self.path_D2_PID = (0.0007, 0.00008, 0.001)
-        self.path_E_PID = (-0.0004, -0.00011, -0.001)
-        self.path_F_PID = (0.0008, 0.00008, 0.003)
-        self.path_G_PID = (-0.00035, -0.00010, -0.001)
-        self.path_H_PID = (0.001, 0.00003, 0.001)
-        #self.path_I_PID = (0.005, 0.00010, 0.0019)
-        self.path_I_PID = (0.001, 0.00004, 0.0009)
-        self.path_J_PID = (-0.001, -0.00009, -0.0008)
-        self.path_K_PID = (0.0004, 0.00005, 0.005)
-
-        #PID values of each circle, each element is a tuple (kp, ki, kd)
-        self.circle_a_PID = (-0.55, -0.001, -0.037)
-        self.circle_b_PID = (-0.05, -0.02, -0.037)
-        self.circle_c_PID = (-0.56, -0.001, -0.037)
-        self.circle_d_PID = (-0.0005, -0.001, -0.003)
-        self.circle_e_PID = (-0.030, -0.000045, -0.0017)
-        self.circle_f_PID = (-0.050, -0.00045, -0.037)
-        self.circle_g_PID = (-0.050, -0.00045, -0.037)
-        self.circle_h_PID = (-0.06, -0.09, -0.02)
-        self.circle_i_PID = (-0.050, -0.00045, -0.037)
-        self.circle_j_PID = (-0.050, -0.00045, -0.037)
-        self.circle_k_PID = (-0.06, -0.00015, -0.01)
-        self.circle_l_PID = (-0.01, -0.09, -0.01)
-        self.circle_m_PID = (-0.06, -0.09, -0.02)
-        self.circle_n_PID = (-0.050, -0.00045, -0.037)
-        self.circle_o_PID = (-0.050, -0.035, -0.037)
-        self.circle_p_PID = (-0.080, -0.02, -0.01)
-        self.circle_q_PID = (-0.050, -0.009, -0.01)
-        self.circle_r_PID = (-0.050, -0.00045, -0.037)
-        self.circle_s_PID = (-0.050, -0.00045, -0.037)
-        self.circle_t_PID = (-0.050, -0.00045, -0.037)
-        self.circle_u_PID = (-0.050, -0.00045, -0.037)
 
         if isMain and enter == 0 and exit == 0: #if the limo runs along the main path
             #array to store all points at which the limo needs to turn, in order of traversal
